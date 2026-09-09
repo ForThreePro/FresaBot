3d1960b828fe6290d510db653b8ef2230464e659
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 15:40:10 -0500

    Actualizar tools-upp.js

--- plugins/tools-upp.js
+++ plugins/tools-upp.js
@@ -5,7 +5,7 @@ import { fileTypeFromBuffer } from "file-type"
 let handler = async (m, { conn }) => {
   let q = m.quoted? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, `🐱 *𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟* 🐱
+  if (!mime) return conn.reply(m.chat, `🍓 *𝗙𝗥𝗘𝗦𝗜𝗧𝗔 𝗕𝗢𝗧* 🍓
 
 *━━━━━━━━━━*
 *⚠️ ERROR DE USO ⚠️*
@@ -15,8 +15,8 @@ let handler = async (m, { conn }) => {
 *➤* Formatos: *Imagen | Video | Audio | Doc*
 
 *━━━━━━━━━━*
-*Owner:* @whois.yallico 
-*WhatsApp:* +51 927 174 369`, m)
+*Owner:* @56927308426 
+*WhatsApp:* +56 9 2730 8426`, m)
 
   try {
     await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } })
@@ -24,9 +24,9 @@ let handler = async (m, { conn }) => {
     let link = await myCloud(media)
     if (!link.url) throw new Error()
 
-    let txt = `🐱 *𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟* 🐱
+    let txt = `🍓 *𝗙𝗥𝗘𝗦𝗜𝗧𝗔 𝗕𝗢𝗧* 🍓
 
-*━━━━━━━━━━━━━━━━━━*
+*━━━━━━━━━━*
 *✅ ARCHIVO SUBIDO CORRECTAMENTE*
 
 *📊 DATOS DEL ARCHIVO*
@@ -34,19 +34,19 @@ let handler = async (m, { conn }) => {
 *➤ ID:* ${link.id || 'N/A'}
 *➤ Peso:* ${formatBytes(media.length)}
 *➤ Servidor:* *evogb.win*
-*➤ Bot:* ***Garfield Bot Oficial***
+*➤ Bot:* ***FRESITA BOT***
 
 *━━━━━━━━━━━━━━━━━━*
-*Owner:* @whois.yallico 
-*WhatsApp:* +51 927 174 369
-> _"Guardado en la nube por Garfield Bot"_ ☁️⚡`
+*Owner:* @56927308426 
+*WhatsApp:* +56 9 2730 8426
+> _"Guardado en la nube por Fresita Bot"_ ☁️💖`
 
-    await conn.sendFile(m.chat, media, 'garfield.' + link.url.split('.').pop(), txt, m)
+    await conn.sendFile(m.chat, media, 'fresita.' + link.url.split('.').pop(), txt, m)
     await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
   } catch (e) {
     console.error(e)
     await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } })
-    await conn.reply(m.chat, `🐱 *𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟* 🐱
+    await conn.reply(m.chat, `🍓 *𝗙𝗥𝗘𝗦𝗜𝗧𝗔 𝗕𝗢𝗧* 🍓
 
 *━━━━━━━━━━*
 *❌ ERROR DE SUBIDA ❌*
@@ -56,8 +56,8 @@ let handler = async (m, { conn }) => {
 *➤* Intenta con otro archivo
 
 *━━━━━━━━━━*
-*Owner:* @whois.yallico 
-*WhatsApp:* +51 927 174 369`, m)
+*Owner:* @56927308426 
+*WhatsApp:* +56 9 2730 8426`, m)
   }
 }
 
