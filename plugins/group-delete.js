a0d8c39e5a902f6e8a70b2e3a4a8af0fccec3fc2
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 15:18:41 -0500

    Actualizar group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+if (!m.quoted) return conn.reply(m.chat, `🍓 𓆩 ***𝗙𝗥𝗘𝗦𝗜𝗧𝗔 𝗕𝗢𝗧*** 𓆪 🍓
 
 *Uso:* Responde al mensaje que deseas eliminar con *${usedPrefix + command}*`, m)
 
@@ -8,9 +8,11 @@ try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
 await m.react('🗑️')
+await conn.reply(m.chat, `🍓 𓆩 ***𝗠𝗘𝗡𝗦𝗔𝗝𝗘 𝗘𝗟𝗜𝗠𝗜𝗡𝗔𝗗𝗢*** 𓆪 🍓\n\n💖 *Listo, borré el mensaje*`, m)
 return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
  } catch {
 await m.react('🗑️')
+await conn.reply(m.chat, `🍓 𓆩 ***𝗠𝗘𝗡𝗦𝗔𝗝𝗘 𝗘𝗟𝗜𝗠𝗜𝗡𝗔𝗗𝗢*** 𓆪 🍓\n\n💖 *Listo, borré el mensaje*`, m)
 return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 }
 }
